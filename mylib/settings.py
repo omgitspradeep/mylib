436173b5f3fcbd69b7c5412cb0a4f6056d0b76c3
omgitspradeep <63057831+omgitspradeep@users.noreply.github.com>
2021-07-05 12:33:49 +0545

    Update settings.py

--- mylib/settings.py
+++ mylib/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vefweo08v%wlxn^0pw+6_wk%1!ydf&l*1bww(w8wz6b&gp-8gd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
